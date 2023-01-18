d6e4d42089e3945983ed3d077286f3fc46f38753
jjhuang01 <jjhuang01@outlook.com>
2023-01-18 14:30:22 +0800

    push

--- src/App.vue
+++ src/App.vue
@@ -10,32 +10,34 @@
     </el-card>
 
     <div>
-    <el-form label-position="top" :inline="true"  class="grid grid-cols-3 gap-2 m-5">
+      <el-form label-position="top" :inline="true" class="grid grid-cols-3 gap-2 m-5">
         <el-form-item label="label">
-          <textarea class="outline-gray-500 bg-zinc-100 col-span-1" ring v-model="data.label" name="" id="" cols="30" rows="10"
-        placeholder="label">
+          <textarea  @blur="submit" @input="submit" class="outline-gray-500 bg-zinc-100 col-span-1" ring v-model="data.label" name="" id="" cols="30"
+            rows="10" placeholder="label">
               </textarea>
         </el-form-item>
-        <el-form-item label="value" v-show="radio==3">
-          <textarea class="outline-gray-500 bg-zinc-100 col-span-1" ring name="" v-model="data.value" id="" cols="30" rows="10"
-        placeholder="value"></textarea>
+        <el-form-item label="value" v-show="radio == 3">
+          <textarea @blur="submit" @input="submit" class="outline-gray-500 bg-zinc-100 col-span-1" ring name="" v-model="data.value" id="" cols="30"
+            rows="10" placeholder="value"></textarea>
         </el-form-item>
-        <el-form-item label="结果(点击复制)" class="cursor-pointer">
-         <pre class="bg-zinc-100 p-4">
+        <el-form-item label="结果(点击复制)" class="cursor-pointer" @click="copyHandle">
+          <pre class="bg-zinc-100 p-4">
 {{ (preData) }}
          </pre>
         </el-form-item>
-    </el-form>
+      </el-form>
     </div>
 
-    <div>
-      <button class="basis-full bg-blue-500 block w-screen-sm mx-auto text-neutral-50 py-4 rounded-lg cursor-pointer" type="primary" size="large" @click="submit">确定</button>
-    </div>
+    <!-- <div>
+      <button class="basis-full bg-blue-500 block w-screen-sm mx-auto text-neutral-50 py-4 rounded-lg cursor-pointer"
+        type="primary" size="large" @click="submit">确定</button>
+    </div> -->
   </div>
 </template>
-<script setup>
-import { js_beautify, css_beautify, html_beautify } from 'js-beautify'
-import { ref, reactive } from 'vue'
+<script setup lang="ts">
+import { ref, reactive,getCurrentInstance } from 'vue'
+import {ElMessage} from 'element-plus'
+const { appContext } = getCurrentInstance()!
 let radio = ref('1')
 let data = reactive({
   label: [],
@@ -71,15 +73,15 @@ function submit() {
     })
     preData.value = tmp
 
-  }else if(_type ==4){
+  } else if (_type == 4) {
     let label = data.label.trim()
     let res = label.split(/[(\r\n\s)\r\n\s]+/)
-   
+
     let map = {}
-    res.forEach((item,i)=>{
-      map[item]=''
+    res.forEach((item, i) => {
+      map[item] = ''
     })
-    preData.value =  map
+    preData.value = map
   }
   // var reg = /\n(\n)*( )*(\n)*\n/g;
   // let label = data.label.trim()
@@ -88,8 +90,38 @@ function submit() {
   // console.log(label.split(/[(\r\n\s)\r\n\s]+/))
 }
 
-function change(e){
+function change(e) {
   radio.value = e
   submit()
+}
+
+function copyHandle() {
+  var text = preData.value
+  if (navigator.clipboard) {
+    // clipboard api 复制
+    navigator.clipboard.writeText(text);
+    ElAlert({title:'复制成功'})
+  } else {
+    var textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    // 隐藏此输入框
+    textarea.style.position = 'fixed';
+    textarea.style.clip = 'rect(0 0 0 0)';
+    textarea.style.top = '10px';
+    // 赋值
+    textarea.value = text;
+    // 选中
+    textarea.select();
+    // 复制
+    document.execCommand('copy', true);
+    // 移除输入框
+    document.body.removeChild(textarea);
+    ElMessage({
+      type:'success',
+      message:'复制成功'
+    },appContext)
+
+  }
+
 }
 </script>
